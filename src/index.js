69057c5be916504693aac622812f172ac22a7461
kingvictorvector <jcjaneway1@gmail.com>
2025-06-18 14:13:54 -0700

    Cleanup: Remove src folder, update server.cjs for LAN, and general project simplification

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// Re-export everything from the client index
-export * from './client/index.tsx';
-
-// Also import the default export if there is one
-import DefaultExport from './client/index.tsx';
-export default DefaultExport; 
